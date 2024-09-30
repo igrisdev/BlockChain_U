610144d16895470303de959c865669869abf2679
igrisdev <johanalvarezz237@gmail.com>
2024-09-29 20:57:36 -0500

    antes del apagon xd

--- client/src/env.d.ts
+++ client/src/env.d.ts
@@ -1,1 +1,28 @@
 /// <reference path="../.astro/types.d.ts" />
+
+export type CadenaCelulares = {
+  ok:     boolean;
+  listar: Listar[];
+}
+
+export type Listar = {
+  data:         Data;
+  index:        number;
+  previousHash: string;
+  hash:         string;
+  date:         Date;
+}
+
+export type Data = {
+  estaReportado: boolean;
+  propietario?:  Propietario;
+  imei?:         number;
+  modelo?:       string;
+  marca?:        string;
+  precio?:       number;
+}
+
+export type Propietario = {
+  id_propietario: number;
+  nombres:        string;
+}
